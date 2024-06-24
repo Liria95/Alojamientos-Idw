f27bd788d69380c8b4fd0ad6bd5900b1d512407e
liriaolivera20@gmail.com <liriaolivera20@gmail.com>
2024-06-24 19:16:08 -0300

    agregado icono a footer

--- src/alojamientos/src/paginas/Home/componentes/Footer.jsx
+++ src/alojamientos/src/paginas/Home/componentes/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebook, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import logo from '../../assets/casalogo.png';
 import '../css/Footer.css';
 
@@ -7,9 +9,21 @@ export function Footer() {
         <footer>
             <div className="redesSociales">
                 <ul>
-                    <li><a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Instagram">Instagram</a></li>
-                    <li><a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">Facebook</a></li>
-                    <li><a href="https://wa.me" target="_blank" rel="noopener noreferrer" aria-label="Whatsapp">Whatsapp</a></li>
+                    <li>
+                        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+                            <FontAwesomeIcon icon={faInstagram} /> Instagram
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+                            <FontAwesomeIcon icon={faFacebook} /> Facebook
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://wa.me" target="_blank" rel="noopener noreferrer" aria-label="Whatsapp">
+                            <FontAwesomeIcon icon={faWhatsapp} /> Whatsapp
+                        </a>
+                    </li>
                 </ul>
                 <img src={logo} width="150" alt="Logo de la empresa" />
             </div>
