70ea474f51df551ec844118affd7cb5f4f714687
liriaolivera20@gmail.com <liriaolivera20@gmail.com>
2024-06-25 15:52:00 -0300

    nuevo envio de card destacadas

--- src/alojamientos/src/paginas/Home/componentes/CardDestacada.jsx
+++ src/alojamientos/src/paginas/Home/componentes/CardDestacada.jsx
@@ -1,21 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/CardDestacada.css';
 import casa from '../../assets/casa-portada.png';
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
-export function CardDestacada({nombre}) {
-
+export function CardDestacada({ nombre }) {
     const [alojamientos, setAlojamientos] = useState([]);
     const [imagenes, setImagenes] = useState([]);
-    // const [currentImagen, setCurrentImagen] = useState(null);
 
-    // useEffect para cargar alojamientos e imágenes al montar el componente
     useEffect(() => {
         fetchAlojamientos();
         fetchImagenes();
     }, []);
 
-    // Función para obtener los alojamientos del backend
     const fetchAlojamientos = async () => {
         const endpoint = 'http://localhost:3001/alojamiento/getAlojamientos';
         try {
@@ -32,7 +31,6 @@ export function CardDestacada({nombre}) {
         }
     };
 
-    // Función para obtener las imágenes del backend
     const fetchImagenes = async () => {
         const endpoint = 'http://localhost:3001/imagen/getAllImagenes';
         try {
@@ -40,7 +38,6 @@ export function CardDestacada({nombre}) {
             if (response.ok) {
                 const data = await response.json();
                 setImagenes(data);
-
             } else {
                 throw new Error('Error al cargar las imágenes');
             }
@@ -50,49 +47,44 @@ export function CardDestacada({nombre}) {
         }
     };
 
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000
+    };
 
-    //Trayendo la info a traves de los alojamientos
     return (
         <div className="destacadasCards">
-
             {alojamientos.length > 0 ? (
-
-                alojamientos.map((alojamiento) =>
-
-                    imagenes.length >0 ? (
-
-                        imagenes.map((imagen) =>
-
-                            imagen.idAlojamiento === alojamiento.idAlojamiento ? (
-                                <div className="cdest" key={alojamiento.idAlojamiento}>
-                                    <h4><a href="#">{alojamiento.Titulo}</a></h4>
-                                    <img src={imagen.RutaArchivo} alt={`Imagen ${alojamiento.Titulo}`}/>
-                                    <p className="infoPropiedadDestacada">
-                                        <span className="enfasisTexto">Lo mejor de la propiedad: Lorem ipsum dolor sit amet! Officiis.</span>
-                                    </p>
-                                </div>
-                            ) : (
-                                <div className="cdestNoVisible" key={alojamiento.idAlojamiento}></div>
-                            )
-                        )
-
-                    ) : (
+                alojamientos.slice(0, 3).map((alojamiento) => {
+                    const imagenesAlojamiento = imagenes.filter(img => img.idAlojamiento === alojamiento.idAlojamiento);
+                    return (
                         <div className="cdest" key={alojamiento.idAlojamiento}>
                             <h4><a href="#">{alojamiento.Titulo}</a></h4>
-                            <img src="" alt=" sin imagen para mostrar"/>
+                            {imagenesAlojamiento.length > 0 ? (
+                                <Slider {...settings}>
+                                    {imagenesAlojamiento.map((imagen, index) => (
+                                        <div key={index}>
+                                            <img src={imagen.RutaArchivo} alt={`Imagen ${alojamiento.Titulo}`} />
+                                        </div>
+                                    ))}
+                                </Slider>
+                            ) : (
+                                <img src={casa} alt={`Imagen ${alojamiento.Titulo}`} />
+                            )}
                             <p className="infoPropiedadDestacada">
                                 <span className="enfasisTexto">Lo mejor de la propiedad: Lorem ipsum dolor sit amet! Officiis.</span>
                             </p>
                         </div>
-                    )
-                )
-
+                    );
+                })
             ) : (
                 <p>No hay imágenes disponibles.</p>
             )}
-
         </div>
-
     );
-
 }
--- src/alojamientos/src/paginas/Institucional/componentes/SeccionPorQueElegirnos.jsx
+++ src/alojamientos/src/paginas/Institucional/componentes/SeccionPorQueElegirnos.jsx
@@ -1,30 +1,57 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../css/SeccionPorQueElegirnos.css';
-import imagen_empresa1 from '../../assets/imagen_empresa1.jpg';
+import imagen_empresa1 from '../../assets/conocimiento.jpg';
+import imagen_empresa2 from '../../assets/experiencia.jpg';
+import imagen_empresa3 from '../../assets/servicioalcliente.jpg';
+import imagen_empresa4 from '../../assets/negociacion.jpg';
+import imagen_empresa5 from '../../assets/contactos.jpg';
+import imagen_empresa6 from '../../assets/herramientas.jpg';
+
 
 function PorQueElegirnos() {
+  const imagenes = [
+    [imagen_empresa1, imagen_empresa2, imagen_empresa3], // Grupo de imágenes para la primera iteración
+    [imagen_empresa4, imagen_empresa5, imagen_empresa6],
+  ];
+
   return (
     <div className="PorQueElegirnos">
-      {[1, 2, 3].map((i) => (
-        <div key={i} className={`container container${i}`}>
-          <img src={imagen_empresa1} alt={`Empresa XYZ ${i}`} className={`imagen-empresa imagen-${i}`} />
+      {imagenes.map((grupo, index) => (
+        <div key={index} className={`container container${index + 1}`}>
+          <Carousel
+            showThumbs={false}
+            emulateTouch={true}
+            infiniteLoop={true}
+            transitionTime={500} // Tiempo de transición en milisegundos
+            swipeable={true} // Permite deslizar con el dedo en dispositivos táctiles
+            autoPlay={true} // Activa el deslizamiento automático
+            interval={3000} // Intervalo de tiempo en milisegundos entre cada deslizamiento
+          >
+            {grupo.map((imagen, i) => (
+              <div key={i}>
+                <img src={imagen} alt={`Empresa XYZ ${index + 1}`} className={`imagen-empresa imagen-${index + 1}`} />
+              </div>
+            ))}
+          </Carousel>
           <div className="barras">
             <div className="barra-superior">
               <p>Texto 1</p>
               <div className="barra-superior-adicional">
-                <p>{i === 1 ? '90%' : i === 2 ? '89%' : '80%'}</p>
+                <p>{index === 0 ? '90%' : index === 1 ? '89%' : '80%'}</p>
               </div>
             </div>
             <div className="barra-media">
               <p>Texto 2</p>
               <div className="barra-media-adicional">
-                <p>{i === 1 ? '76%' : i === 2 ? '75%' : '100%'}</p>
+                <p>{index === 0 ? '76%' : index === 1 ? '75%' : '100%'}</p>
               </div>
             </div>
             <div className="barra-inferior">
               <p>Texto 3</p>
               <div className="barra-inferior-adicional">
-                <p>{i === 1 ? '99%' : i === 2 ? '95%' : '85%'}</p>
+                <p>{index === 0 ? '99%' : index === 1 ? '95%' : '85%'}</p>
               </div>
             </div>
           </div>
